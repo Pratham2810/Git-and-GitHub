782bcc330e7d07e2a5e3425890bbd2d4eeeaf979
Pratham Priyanshu Mohanty <prathammohanty18@gmail.com>
2024-04-26 11:30:12 +0530

    Add files via upload

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,68 +1,26 @@
 #!/bin/bash
+# This script calculates simple interest given principal, annual rate of interest and time period in years.
+# Do not use this in production. Sample purpose only.
 
-# This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ $# != 2 ]]
-then
-  echo "backup.sh target_directory_name destination_directory_name"
-  exit
-fi
+# Author: Upkar Lidder (IBM)
+# Addtional Authors:
+# <your Github username>
 
-# This checks if argument 1 and argument 2 are valid directory paths
-if [[ ! -d $1 ]] || [[ ! -d $2 ]]
-then
-  echo "Invalid directory path provided"
-  exit
-fi
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
 
-# [TASK 1]
-targetDirectory=$1
-destinationDirectory=$2
+# Output:
+# simple interest = p*t*r
 
-# [TASK 2]
-echo "targetDirectory: $targetDirectory"
-echo "destinationDirectory: $destinationDirectory"
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
 
-# [TASK 3]
-currentTS=`date +%s`
-# [TASK 4]
-backupFileName="backup-$currentTS.tar.gz"
-
-# We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
-
-# To make things easier, we will define some useful variables...
-
-# [TASK 5]
-origAbsPath=`pwd`
-
-# [TASK 6]
-cd $destinationDirectory
-destAbsPath=`pwd`
-
-# [TASK 7]
-cd $origAbsPath
-cd $targetDirectory
-
-# [TASK 8]
-yesterdayTS=$(($currentTS - 24*60*60))
-
-declare -a toBackup
-
-for file in $(ls) # [TASK 9]
-do
-  # [TASK 10]
-  if ((`date -r $file +%s` > $yesterdayTS))
-  then
-    # [TASK 11]
-    toBackup+=($file)
-  fi
-done
-
-# [TASK 12]
-tar -czvf $backupFileName ${toBackup[@]}
-# [TASK 13]
-mv $backupFileName $destAbsPath
-# Congratulations! You completed the final project for this course!
+s=$(expr $p \* $t \* $r / 100)
+echo "The simple interest is: "
+echo $s
